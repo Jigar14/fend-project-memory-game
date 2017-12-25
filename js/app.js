5a9e65afb13684af576d34aa9cc7a7a54582ff5f
Jig <pateljigar140795@gmail.com>
2017-12-24 22:51:59 -0500

    Added the css file and some js function signatures.

--- js/app.js
+++ js/app.js
@@ -36,3 +36,15 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function resetGame(){
+     //call this function when the reset button is called
+ }
+
+ function incrementMoveCount(){
+     //function to increment the move count and update the ratings after each count.
+ }
+
+ function updateRatings(moveCount){
+     //function to update the ratings based upon the move count.
+ }
