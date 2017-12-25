aff6eb75ffc73b324b1b04f3ec2eb2302e98062e
Jig <pateljigar140795@gmail.com>
2017-12-24 22:03:15 -0500

    Added the images and basic game rules for ratings

---
+++ js/data.js
@@ -0,0 +1,39 @@
+const cards = [{
+    name: "Selena",
+    image: "img/selena.jpg",
+},{
+    name: "Dhoni",
+    image: "img/dhoni.jpg",
+},{
+    name: "Obama",
+    image: "img/obama.jpg",
+},{
+    name: "Drake",
+    image: "img/drake.jpg",
+},{
+    name: "Shreya",
+    image: "img/shreya.jpg",
+},{
+    name: "Katy",
+    image: "img/katy.jpg",
+},{
+    name: "Rihana",
+    image: "img/rihanna.jpg",
+},{
+    name: "Demi",
+    image: "img/demi.jpg",
+},{
+    name: "Sundar",
+    image: "img/sundar.jpg"
+}
+]
+
+const ratings = [{
+    star:3,
+    movesThreshold:3,
+}, {
+    star:2,
+    movesThreshold:5,
+}]
+
+let moveCounter = 0;
